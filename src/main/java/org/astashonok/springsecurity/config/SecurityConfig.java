ec1fa464e8904981559aa61ac9260f59a00358cf
kastashonok <konstantin.astashonok@gmail.com>
2021-08-08 19:53:28 +0300

    STEP_7-Using-Authentication-Form

--- src/main/java/org/astashonok/springsecurity/config/SecurityConfig.java
+++ src/main/java/org/astashonok/springsecurity/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import static org.astashonok.springsecurity.model.Role.USER;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +15,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -29,7 +31,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .anyRequest()
         .authenticated()
         .and()
-        .httpBasic();
+        .formLogin()
+        .loginPage("/auth/login").permitAll()
+        .defaultSuccessUrl("/auth/success")
+        .and()
+        .logout()
+        .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout", HttpMethod.POST.name()))
+        .invalidateHttpSession(true)
+        .clearAuthentication(true)
+        .deleteCookies("JSESSIONID")
+        .logoutSuccessUrl("/auth/login");
   }
 
   @Bean
